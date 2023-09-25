bacaa61c9e43c19cb91511412f8157d089243373
codermahbub20 <mkrefat5@gmail.com>
2023-09-25 16:09:37 +0600

    Donation item above the 4 and more see the see all button done

--- src/Components/Domain/Donation.jsx
+++ src/Components/Domain/Donation.jsx
@@ -4,6 +4,7 @@ import SubDonation from "./SubDonation";
 const Donation = () => {
 
     const [donation,setDonation] = useState([])
+    const [datalength,setDatalength] = useState(4);
 
     useEffect(() =>{
         const donationItem = JSON.parse(localStorage.getItem("donation"));
@@ -17,8 +18,11 @@ const Donation = () => {
     return (
         <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 w-9/12 mx-auto">
         {
-            donation.map(sub => <SubDonation key={sub.id} sub={sub}></SubDonation>)
+            donation.slice(0,datalength).map(sub => <SubDonation key={sub.id} sub={sub}></SubDonation>)
         }
+         <div className={datalength === donation.length && 'hidden' || "flex mt-5 md:ml-[490px]"}>
+                <button className="px-3 rounded-xl text-xl text-white py-3 bg-green-700" onClick={() => setDatalength(donation.length)}>See All</button>
+            </div>
             
         </div>
     );
